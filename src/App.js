d995666a67eaf328db512972c139485bf5866636
Mit Sananikone <Mitsananikone@gmail.com>
2022-11-11 18:09:43 -0600

    Shopping cart refactored into create-react-app
    Shopping cart add, delete, restock, checkout complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Products from './Components/products';
+import 'bootstrap/dist/css/bootstrap.css';
+
 function App() {
   return (
    <Products />
